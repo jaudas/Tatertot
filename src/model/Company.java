b7b8412fc1fffcaf9d68db8bad861d14b8383e0b
jaudas <jessica.audas@epfedu.fr>
2015-09-10 15:37:58 +0200

    init model

---
+++ src/model/Company.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Company {
+	
+	private Long idCompany;
+	private String nameCompany;
+	
+	public Company(){
+		super();
+	}
+
+	public Long getIdCompany() {
+		return idCompany;
+	}
+
+	public void setIdCompany(Long idCompany) {
+		this.idCompany = idCompany;
+	}
+
+	public String getNameCompany() {
+		return nameCompany;
+	}
+
+	public void setNameCompany(String nameCompany) {
+		this.nameCompany = nameCompany;
+	}
+	
+	
+
+}
---
+++ src/model/Computer.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.sql.Timestamp;
+
+public class Computer {
+	private Long idComputer;
+	private String nameComputer;
+	private Timestamp introduced;
+	private Timestamp discontinued;
+	private Long company_id;
+	
+	public Computer(){
+		
+	}
+
+	public Long getIdComputer() {
+		return idComputer;
+	}
+
+	public void setIdComputer(Long idComputer) {
+		this.idComputer = idComputer;
+	}
+
+	public String getNameComputer() {
+		return nameComputer;
+	}
+
+	public void setNameComputer(String nameComputer) {
+		this.nameComputer = nameComputer;
+	}
+
+	public Timestamp getIntroduced() {
+		return introduced;
+	}
+
+	public void setIntroduced(Timestamp introduced) {
+		this.introduced = introduced;
+	}
+
+	public Timestamp getDiscontinued() {
+		return discontinued;
+	}
+
+	public void setDiscontinued(Timestamp discontinued) {
+		this.discontinued = discontinued;
+	}
+
+	public Long getCompany_id() {
+		return company_id;
+	}
+
+	public void setCompany_id(Long company_id) {
+		this.company_id = company_id;
+	}
+
+	
+}
